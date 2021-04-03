58d0c19f8679ed0ea705847dc48db3aef06f67c3
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-04-04 02:30:39 +0900

    blogサイトの基本情報を更新した

--- src/website-config.ts
+++ src/website-config.ts
@@ -49,14 +49,14 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'taptappun-blog',
+  description: 'This is a taptappun\'s private blog',
   coverImage: 'img/blog-cover.png',
   logo: 'img/ghost-logo.png',
-  lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  lang: 'jp',
+  siteUrl: 'https://taptappun-blog.netlify.app/',
+  facebook: 'https://www.facebook.com/taku.kobayashi.560/',
+  twitter: 'https://twitter.com/taptappun',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
